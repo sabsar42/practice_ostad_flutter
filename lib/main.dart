b4953afd846a6d31416ff782f83a0f726e86249c
Shakib Absar <sabsar42@gmail.com>
2023-09-28 20:34:31 +0600

    - Device preview : device_preview - Size (height, width) : Sizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:practice_ostad_flutter/home_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:live_class_project/home_screen.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode,
+      builder: (context) {
+        return MyApp();
+      },
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +22,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return Sizer(
+        builder: (context, orientation, deviceType) {
+          print(deviceType);
+          return MaterialApp(
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            home: HomeScreen(),
+          );
+        }
     );
   }
 }
