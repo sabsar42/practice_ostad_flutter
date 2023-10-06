3b4b1e87e7712854f14f879b42dda4f057fd9284
Shakib Absar <sabsar42@gmail.com>
2023-10-07 04:06:53 +0600

    Sum Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice_ostad_flutter/home_screen.dart';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
