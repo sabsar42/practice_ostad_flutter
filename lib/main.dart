28657c08210507f33262313fc594d040663be21a
Shakib Absar <sabsar42@gmail.com>
2023-10-07 04:04:39 +0600

    Statefull Widget Lifecycle has been introduced

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:practice_ostad_flutter/home_screen.dart';
-import 'package:device_preview/device_preview.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:live_class_project/home_screen.dart';
-import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      enabled: kDebugMode,
-      builder: (context) {
-        return MyApp();
-      },
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,15 +13,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-        builder: (context, orientation, deviceType) {
-          print(deviceType);
-          return MaterialApp(
-            locale: DevicePreview.locale(context),
-            builder: DevicePreview.appBuilder,
-            home: HomeScreen(),
-          );
-        }
+    return MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
